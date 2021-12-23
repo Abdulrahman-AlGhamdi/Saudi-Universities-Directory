f563550c11c7e3ee7b535a07250e5fba14fded79
Abdulrahman Al-Ghamdi <XxD7ooom.SSxX@Gmail.Com>
2021-12-23 15:42:02 +0300

    Move the api calling to the background thread

--- app/src/main/java/com/ss/universitiesdirectory/repository/universities/UniversitiesRepository.kt
+++ app/src/main/java/com/ss/universitiesdirectory/repository/universities/UniversitiesRepository.kt
@@ -2,7 +2,10 @@ package com.ss.universitiesdirectory.repository.universities
 
 import com.ss.universitiesdirectory.model.UniversityModel
 import com.ss.universitiesdirectory.repository.common.ApiService
+import com.ss.universitiesdirectory.repository.universities.UniversitiesRepository.UniversitiesState.*
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
 import java.util.*
 import javax.inject.Inject
 
@@ -11,18 +14,18 @@ class UniversitiesRepository @Inject constructor(
 ) {
 
     fun getAllUniversities(region: String) = flow {
-        this.emit(UniversitiesState.Loading)
+        this.emit(Loading)
         val language = Locale.getDefault().language
         val response = apiService.getAllUniversities(language, region)
 
-        if (!response.isSuccessful) this.emit(UniversitiesState.Failed(response.message()))
-        else response.body()?.let { this.emit(UniversitiesState.Successful(it)) }
-    }
+        if (!response.isSuccessful) this.emit(Failed(response.message()))
+        else response.body()?.let { this.emit(Successful(it)) }
+    }.flowOn(Dispatchers.IO)
 
     sealed class UniversitiesState {
         object Idle                                                    : UniversitiesState()
         object Loading                                                 : UniversitiesState()
-        data class Successful(val universities: List<UniversityModel>) : UniversitiesState()
         data class Failed(val message: String)                         : UniversitiesState()
+        data class Successful(val universities: List<UniversityModel>) : UniversitiesState()
     }
 }
