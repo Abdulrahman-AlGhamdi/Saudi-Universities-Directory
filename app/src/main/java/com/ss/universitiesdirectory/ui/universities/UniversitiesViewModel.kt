1ecb43dd5f5b6fed81cfaa3172ba51ff981597c7
Abdulrahman Al-Ghamdi <XxD7ooom.SSxX@Gmail.Com>
2022-02-28 08:39:11 +0300

    Adding network change broadcast receiver

--- app/src/main/java/com/ss/universitiesdirectory/ui/universities/UniversitiesViewModel.kt
+++ app/src/main/java/com/ss/universitiesdirectory/ui/universities/UniversitiesViewModel.kt
@@ -1,30 +1,17 @@
 package com.ss.universitiesdirectory.ui.universities
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import com.ss.universitiesdirectory.model.UniversityModel
 import com.ss.universitiesdirectory.repository.universities.UniversitiesRepository
-import com.ss.universitiesdirectory.repository.universities.UniversitiesRepository.UniversitiesState
-import com.ss.universitiesdirectory.repository.universities.UniversitiesRepository.UniversitiesState.Idle
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class UniversitiesViewModel @Inject constructor(
-    private val universitiesRepository: UniversitiesRepository
+    universitiesRepository: UniversitiesRepository
 ) : ViewModel() {
 
     var universities = mutableListOf<UniversityModel>()
-    private var _universitiesState = MutableStateFlow<UniversitiesState>(Idle)
-    val universitiesState = _universitiesState.asStateFlow()
 
-    fun getAllUniversities(region: String = "") = viewModelScope.launch {
-        universitiesRepository.getAllUniversities(region).collect {
-            _universitiesState.value = it
-        }
-    }
+    val universitiesState  = universitiesRepository.universitiesState
 }
