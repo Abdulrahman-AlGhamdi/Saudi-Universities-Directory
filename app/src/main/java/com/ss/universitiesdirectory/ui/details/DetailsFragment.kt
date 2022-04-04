ec5d635a23ed1ecb74b3d918934b6dc1d2ae3ad4
Abdulrahman Al-Ghamdi <XxD7ooom.SSxX@Gmail.Com>
2022-04-04 09:56:12 +0300

    Change details fragment from xml to compose

--- app/src/main/java/com/ss/universitiesdirectory/ui/details/DetailsFragment.kt
+++ app/src/main/java/com/ss/universitiesdirectory/ui/details/DetailsFragment.kt
@@ -4,12 +4,28 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.view.View
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
-import coil.load
+import coil.compose.rememberAsyncImagePainter
 import com.ss.universitiesdirectory.R
 import com.ss.universitiesdirectory.databinding.FragmentDetailsBinding
+import com.ss.universitiesdirectory.ui.theme.*
 import com.ss.universitiesdirectory.utils.navigateTo
 import com.ss.universitiesdirectory.utils.viewBinding
 
@@ -17,81 +33,208 @@ class DetailsFragment : Fragment(R.layout.fragment_details) {
 
     private val binding by viewBinding(FragmentDetailsBinding::bind)
     private val argument by navArgs<DetailsFragmentArgs>()
+    private val directions = DetailsFragmentDirections
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        showData()
-    }
-
-    private fun showData() {
         val university = argument.university
-        val directions = DetailsFragmentDirections
-        binding.detailsLogo.load(university.logo)
-        binding.about.text = university.about
 
-        if (university.colleges.isNotEmpty()) {
-            binding.colleges.visibility = View.VISIBLE
-            binding.colleges.setOnClickListener {
-                val action = directions.actionDetailsFragmentToWebsiteFragment(university.colleges)
-                findNavController().navigateTo(action, R.id.detailsFragment)
+        binding.composeView.apply {
+            this.setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+            this.setContent {
+                Column(modifier = Modifier.padding(all = 16.dp)) {
+                    Image(
+                        painter = rememberAsyncImagePainter(model = university.logo),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(200.dp)
+                    )
+
+                    Column(Modifier.verticalScroll(state = rememberScrollState())) {
+                        Text(
+                            text = getString(R.string.about),
+                            fontSize = 20.sp,
+                            fontWeight = FontWeight.Bold,
+                            modifier = Modifier.padding(bottom = 8.dp)
+                        )
+                        Text(
+                            text = university.about,
+                            fontSize = 15.sp,
+                            modifier = Modifier.padding(bottom = 8.dp)
+                        )
+                        if (university.website.isNotEmpty()) {
+                            Button(
+                                modifier = Modifier.fillMaxWidth(),
+                                colors = ButtonDefaults.buttonColors(backgroundColor = WebsiteColor),
+                                onClick = {
+                                    val action =
+                                        directions.actionDetailsFragmentToWebsiteFragment(university.website)
+                                    findNavController().navigateTo(action, R.id.detailsFragment)
+                                }
+                            ) {
+                                Text(text = getString(R.string.website), color = Color.White)
+                            }
+                        }
+                        if (university.colleges.isNotEmpty()) {
+                            Button(
+                                modifier = Modifier.fillMaxWidth(),
+                                colors = ButtonDefaults.buttonColors(backgroundColor = CollegesColor),
+                                onClick = {
+                                    val action =
+                                        directions.actionDetailsFragmentToWebsiteFragment(university.colleges)
+                                    findNavController().navigateTo(action, R.id.detailsFragment)
+                                }
+                            ) {
+                                Text(text = getString(R.string.colleges), color = Color.White)
+                            }
+                        }
+                        if (university.news.isNotEmpty()) {
+                            Button(
+                                modifier = Modifier.fillMaxWidth(),
+                                colors = ButtonDefaults.buttonColors(backgroundColor = NewsColor),
+                                onClick = {
+                                    val action =
+                                        directions.actionDetailsFragmentToNewsFragment(university.news)
+                                    findNavController().navigateTo(action, R.id.detailsFragment)
+                                }
+                            ) {
+                                Text(text = getString(R.string.news), color = Color.White)
+                            }
+                        }
+                        if (university.application.isNotEmpty()) {
+                            Button(
+                                modifier = Modifier.fillMaxWidth(),
+                                colors = ButtonDefaults.buttonColors(backgroundColor = ApplicationColor),
+                                onClick = {
+                                    startActivity(
+                                        Intent(
+                                            Intent.ACTION_VIEW,
+                                            Uri.parse(university.application)
+                                        )
+                                    )
+                                }
+                            ) {
+                                Text(text = getString(R.string.application), color = Color.White)
+                            }
+                        }
+                        if (university.location.isNotEmpty()) {
+                            Button(
+                                modifier = Modifier.fillMaxWidth(),
+                                colors = ButtonDefaults.buttonColors(backgroundColor = LocationColor),
+                                onClick = {
+                                    startActivity(
+                                        Intent(
+                                            Intent.ACTION_VIEW,
+                                            Uri.parse(university.location)
+                                        )
+                                    )
+                                }
+                            ) {
+                                Text(text = getString(R.string.location), color = Color.White)
+                            }
+                        }
+                        Text(
+                            text = getString(R.string.communication),
+                            fontSize = 20.sp,
+                            fontWeight = FontWeight.Bold,
+                            modifier = Modifier.padding(vertical = 8.dp)
+                        )
+                        Row(modifier = Modifier.padding(horizontal = 16.dp)) {
+                            if (university.instagram.isNotEmpty()) {
+                                Button(
+                                    onClick = { openSocialMedia(university.instagram) },
+                                    colors = ButtonDefaults.buttonColors(backgroundColor = InstagramColor),
+                                    modifier = Modifier
+                                        .weight(weight = 1f)
+                                        .fillMaxWidth()
+                                        .height(50.dp)
+                                        .padding(horizontal = 4.dp)
+                                ) {
+                                    Icon(
+                                        painter = painterResource(id = R.drawable.icon_instagram),
+                                        contentDescription = null,
+                                        tint = White
+                                    )
+                                }
+                            }
+                            if (university.twitter.isNotEmpty()) {
+                                Button(
+                                    onClick = { openSocialMedia(university.twitter) },
+                                    colors = ButtonDefaults.buttonColors(backgroundColor = TwitterColor),
+                                    modifier = Modifier
+                                        .weight(weight = 1f)
+                                        .fillMaxWidth()
+                                        .height(50.dp)
+                                        .padding(horizontal = 4.dp)
+                                ) {
+                                    Icon(
+                                        painter = painterResource(id = R.drawable.icon_twitter),
+                                        contentDescription = null,
+                                        tint = White
+                                    )
+                                }
+                            }
+                            if (university.youtube.isNotEmpty()) {
+                                Button(
+                                    onClick = { openSocialMedia(university.youtube) },
+                                    colors = ButtonDefaults.buttonColors(backgroundColor = YoutubeColor),
+                                    modifier = Modifier
+                                        .weight(weight = 1f)
+                                        .fillMaxWidth()
+                                        .height(50.dp)
+                                        .padding(horizontal = 4.dp)
+                                ) {
+                                    Icon(
+                                        painter = painterResource(id = R.drawable.icon_youtube),
+                                        contentDescription = null,
+                                        tint = White
+                                    )
+                                }
+                            }
+                            if (university.facebook.isNotEmpty()) {
+                                Button(
+                                    onClick = { openSocialMedia(university.facebook) },
+                                    colors = ButtonDefaults.buttonColors(backgroundColor = FacebookColor),
+                                    modifier = Modifier
+                                        .weight(weight = 1f)
+                                        .fillMaxWidth()
+                                        .height(50.dp)
+                                        .padding(horizontal = 4.dp)
+                                ) {
+                                    Icon(
+                                        painter = painterResource(id = R.drawable.icon_facebook),
+                                        contentDescription = null,
+                                        tint = White
+                                    )
+                                }
+                            }
+                            if (university.snapchat.isNotEmpty()) {
+                                Button(
+                                    onClick = { openSocialMedia(university.snapchat) },
+                                    colors = ButtonDefaults.buttonColors(backgroundColor = SnapchatColor),
+                                    modifier = Modifier
+                                        .weight(weight = 1f)
+                                        .fillMaxWidth()
+                                        .height(50.dp)
+                                        .padding(horizontal = 4.dp)
+                                ) {
+                                    Icon(
+                                        painter = painterResource(id = R.drawable.icon_snapchat),
+                                        contentDescription = null,
+                                        tint = White
+                                    )
+                                }
+                            }
+                        }
+                    }
+                }
             }
-        }
-        if (university.news.isNotEmpty()) {
-            binding.rss.visibility = View.VISIBLE
-            binding.rss.setOnClickListener {
-                val action = directions.actionDetailsFragmentToNewsFragment(university.news)
-                findNavController().navigateTo(action, R.id.detailsFragment)
-            }
-        }
-        if (university.website.isNotEmpty()) {
-            binding.website.visibility = View.VISIBLE
-            binding.website.setOnClickListener {
-                val action = directions.actionDetailsFragmentToWebsiteFragment(university.website)
-                findNavController().navigateTo(action, R.id.detailsFragment)
-            }
-        }
-        if (university.application.isNotEmpty()) {
-            binding.detailsApplication.visibility = View.VISIBLE
-            binding.detailsApplication.setOnClickListener {
-                startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(university.application)))
-            }
-        }
-        if (university.location.isNotEmpty()) {
-            binding.detailsLocation.visibility = View.VISIBLE
-            binding.detailsLocation.setOnClickListener {
-                startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(university.location)))
-            }
-        }
-        if (university.twitter.isNotEmpty()) {
-            binding.communicationHeader.visibility = View.VISIBLE
-            binding.twitter.visibility = View.VISIBLE
-            binding.twitter.setOnClickListener { openSocialMedia(university.twitter) }
-        }
-        if (university.facebook.isNotEmpty()) {
-            binding.communicationHeader.visibility = View.VISIBLE
-            binding.facebook.visibility = View.VISIBLE
-            binding.facebook.setOnClickListener { openSocialMedia(university.facebook) }
-        }
-        if (university.youtube.isNotEmpty()) {
-            binding.communicationHeader.visibility = View.VISIBLE
-            binding.youtube.visibility = View.VISIBLE
-            binding.youtube.setOnClickListener { openSocialMedia(university.youtube) }
-        }
-        if (university.instagram.isNotEmpty()) {
-            binding.communicationHeader.visibility = View.VISIBLE
-            binding.instagram.visibility = View.VISIBLE
-            binding.instagram.setOnClickListener { openSocialMedia(university.instagram) }
-        }
-        if (university.snapchat.isNotEmpty()) {
-            binding.snapchat.visibility = View.VISIBLE
-            binding.communicationHeader.visibility = View.VISIBLE
-            binding.snapchat.setOnClickListener { openSocialMedia(university.snapchat) }
         }
     }
 
     private fun openSocialMedia(stringUri: String) {
-        val intent  = Intent(Intent.ACTION_VIEW, Uri.parse(stringUri))
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(stringUri))
         val chooser = Intent.createChooser(intent, "Open app")
         startActivity(chooser)
     }
---
+++ app/src/main/java/com/ss/universitiesdirectory/ui/theme/Color.kt
@@ -0,0 +1,26 @@
+package com.ss.universitiesdirectory.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Application Colors
+val PrimaryColor = Color(0xFF4e342e)
+val DarkPrimaryColor = Color(0xFF260e04)
+val AccentColor = Color(0xFE7b5e57)
+
+val Black = Color(0xFF000000)
+val White = Color(0xFFFFFFFF)
+val DarkBackgroundColor = Color(0x00121212)
+
+// University Information Colors
+val WebsiteColor = Color(0xFFe84c3d)
+val ApplicationColor = Color(0xFFbb6883)
+val LocationColor = Color(0xFF00949b)
+val CollegesColor = Color(0xFF576f9e)
+val NewsColor = Color(0xFFe6a835)
+
+// Social Media Colors
+val TwitterColor = Color(0xFF1da1f2)
+val YoutubeColor = Color(0xFFED1D24)
+val SnapchatColor = Color(0xFFfffc00)
+val FacebookColor = Color(0xFF1877f2)
+val InstagramColor = Color(0xFFC837AB)
