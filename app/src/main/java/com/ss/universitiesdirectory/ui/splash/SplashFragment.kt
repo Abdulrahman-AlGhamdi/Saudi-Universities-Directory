f132ce0fe4f42c4954a4acd8e5c34f743c574cfe
Abdulrahman Al-Ghamdi <XxD7ooom.SSxX@Gmail.Com>
2022-04-07 12:03:32 +0300

    Convert splash screen ui from XML to compose

--- app/src/main/java/com/ss/universitiesdirectory/ui/splash/SplashFragment.kt
+++ app/src/main/java/com/ss/universitiesdirectory/ui/splash/SplashFragment.kt
@@ -2,38 +2,56 @@ package com.ss.universitiesdirectory.ui.splash
 
 import android.os.Bundle
 import android.view.View
-import android.view.animation.AnimationUtils
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.compose.ui.res.painterResource
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import com.ss.universitiesdirectory.R
 import com.ss.universitiesdirectory.databinding.FragmentSplashBinding
 import com.ss.universitiesdirectory.utils.navigateTo
 import com.ss.universitiesdirectory.utils.viewBinding
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 
 class SplashFragment : Fragment(R.layout.fragment_splash) {
 
     private val binding by viewBinding(FragmentSplashBinding::bind)
+    private val directions = SplashFragmentDirections
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        startSplash()
-        endSplash()
-    }
+        binding.composeView.apply {
+            this.setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+            this.setContent {
+                val scale = remember { Animatable(0f) }
 
-    private fun startSplash() {
-        binding.logo.animation  = AnimationUtils.loadAnimation(context, R.anim.from_bottom_centered)
-        binding.title.animation = AnimationUtils.loadAnimation(context, R.anim.from_bottom)
-    }
+                Box(contentAlignment = Alignment.Center, modifier = Modifier.fillMaxSize()) {
+                    Image(
+                        painter = painterResource(id = R.drawable.icon_university),
+                        contentDescription = null,
+                        modifier = Modifier.scale(scale.value)
+                    )
+                }
 
-    private fun endSplash() = lifecycleScope.launch(Dispatchers.Main) {
-        delay(1100)
-        val directions = SplashFragmentDirections
-        val action     = directions.actionSplashFragmentToUniversitiesFragment()
-        findNavController().navigateTo(action, R.id.splashFragment)
+                LaunchedEffect(key1 = true) {
+                    scale.animateTo(
+                        targetValue = 0.8f,
+                        animationSpec = tween(durationMillis = 1000)
+                    )
+
+                    val action = directions.actionSplashFragmentToUniversitiesFragment()
+                    findNavController().navigateTo(action, R.id.splashFragment)
+                }
+            }
+        }
     }
 }
