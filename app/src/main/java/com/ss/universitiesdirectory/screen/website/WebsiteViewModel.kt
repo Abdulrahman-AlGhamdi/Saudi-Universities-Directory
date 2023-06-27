8789aba098a07a7f9c41106694b51df002bab6c3
Abdulrahman Al-Ghamdi <abdulrahman@Abdulrahmans-MacBook-Air.local>
2023-06-27 10:04:33 +0300

    Fix website crash

--- app/src/main/java/com/ss/universitiesdirectory/screen/website/WebsiteViewModel.kt
+++ app/src/main/java/com/ss/universitiesdirectory/screen/website/WebsiteViewModel.kt
@@ -2,8 +2,10 @@ package com.ss.universitiesdirectory.screen.website
 
 import androidx.lifecycle.ViewModel
 import com.ss.universitiesdirectory.repository.website.WebsiteRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
+@HiltViewModel
 class WebsiteViewModel @Inject constructor(
     private val websiteRepository: WebsiteRepository
 ) : ViewModel() {
